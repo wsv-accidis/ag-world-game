d07865deb30f2ec075fc4a937f82dc6d73daeda0
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2024-11-05 16:38:39 +0100

    Kneippbyn tileset and map work.

--- tiled/tileset-kneippbyn.tsx
+++ tiled/tileset-kneippbyn.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Kneippbyn" tilewidth="32" tileheight="32" tilecount="810" columns="18">
- <image source="tileset-kneippbyn.png" width="576" height="1440"/>
+<tileset version="1.10" tiledversion="1.11.0" name="Kneippbyn" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
+ <image source="tileset-kneippbyn.png" width="1024" height="2048"/>
 </tileset>
